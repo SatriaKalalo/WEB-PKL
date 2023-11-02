68faa4edf370617dab078a20d2f1e7b6a213ed3b
SatriaKalalo <112679772+SatriaKalalo@users.noreply.github.com>
2023-11-02 14:20:18 +0700

    Add files via upload

---
+++ Percobaan.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Percobaan.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <div class="box">
+        <form>
+            <h2>Sign in</h2>
+            <input type="text" placeholder="Username">
+            <input type="passsword" placeholder="Password">
+            <input type="sumbit" value="Login">
+            <div class="group">
+                <a href="#">Forget Password</a>
+                <a href="#">Sign Up</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
