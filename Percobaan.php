417155832657f1034673ab6ee3a6291c04122857
SatriaKalalo <112679772+SatriaKalalo@users.noreply.github.com>
2023-11-09 07:49:23 +0700

    Add files via upload

--- Percobaan.php
+++ Percobaan.php
@@ -7,19 +7,63 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <span></span>
+        <span></span>
+        <span></span>
+        <form id="signinform">
+            <h2>Login</h2>
+            <div class="inputbox">
+                <input type="text" placeholder="Username">
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder="Password">
+            </div>
+            <div class="inputbox group">
+                <a href="#">Lupa Password</a>
+                <a href="#" id="signup">Sign Up</a>
+            </div>
+            <div class="inputbox">
+                <input type="submit" value="Sign In">
+            </div>
+        </form>
 
-    <div class="box">
-        <form>
-            <h2>Sign in</h2>
-            <input type="text" placeholder="Username">
-            <input type="passsword" placeholder="Password">
-            <input type="sumbit" value="Login">
-            <div class="group">
-                <a href="#">Forget Password</a>
-                <a href="#">Sign Up</a>
+        <form id="signupform">
+            <h2>Registrasi</h2>
+            <div class="inputbox">
+                <input type="text" placeholder="Username">
+            </div>
+            <div class="inputbox">
+                <input type="text" placeholder=" Alamat Email">
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder=" Bikin Password">
+            </div>
+            <div class="inputbox">
+                <input type="password" placeholder=" Konfirmasi Password">
+            </div>
+            <div class="inputbox">
+                <input type="submit" value="Sign up">
+            </div>
+            <div class="inputbox group">
+                <a href="#">Apakah Anda Punya Akun ?
+                    <b id="signin">
+                            Login
+                    </b>
+                </a>
             </div>
         </form>
     </div>
+    <script>
+        let signup = document.querySelector('#signup');
+        let signin = document.querySelector('#signin');
+        let body = document.querySelector('body');
+        signup.onclick = function(){
+          body.classList.add('signup');
+        }
+        signin.onclick = function(){
+          body.classList.remove('signup');
+        }
+    </script>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="wrapper">
+            <div class="brand">
+                <div class="Nama1">smkmutuckp</div>
+                <div class="Nama2">.id</div>
+            </div>
+            <ul class="navigasi">
+                <li><a class="teks1" href="#">Home</a></li>
+                <li><a class="teks2" href="#" class="active">Collection</a></li>
+                <li><a class="teks3" href="#">history</a></li>
+                <li><img src="BHXr.gif" alt="Hack"></li>
+            </ul>
+        </nav>
+        <div class="GambarHack">
+            <div style="background-color:'ivory';"></div>
+        </div>
+    </div>
+    <div class="wrapper1">
+        <div class="carouse1">
+            <img src="LOGO_PPLG-removebg-preview.PNG" alt="RPL">
+            <img src="XI RPL.PNG" alt="TKJ">
+            <img src="XI RPL.PNG" alt="TKR">
+            <img src="XI RPL.PNG" alt="TBSM">
+            <img src="XI RPL.PNG" alt="TO">
+            <img src="XI RPL.PNG" alt="MPLB">
+            <img src="XI RPL.PNG" alt="TMI">
+        </div>
+    </div>
+</body>
+</html>
